48dc2f2261fbbc2969647681d226ed44d2cd66b2
Naranbaatar Bayaraagiin <smth.781029@gmail.com>
2015-10-30 20:31:21 +0800

    Initial Commit

---
+++ TourProject_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TourProject_2
+//
+//  Created by Naranbaatar Bayaraagiin on 10/30/15.
+//  Copyright © 2015 Naranbaatar Bayaraagiin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
